f9dc7b5b7f23c5c9a104eb255db05a0724c74e06
Argenis <argenismuga@gmail.com>
2026-01-24 20:49:12 -0600

    Primer commit

---
+++ src/main/java/com/washtrack/washtrack_api/WashtrackApiApplication.java
@@ -0,0 +1,13 @@
+package com.washtrack.washtrack_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashtrackApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashtrackApiApplication.class, args);
+	}
+
+}
