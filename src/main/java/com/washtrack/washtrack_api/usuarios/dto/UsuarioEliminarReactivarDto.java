eca105ae9e1a06f30b93dff2b8f8918e05d44a5e
Argenis <argenismuga@gmail.com>
2026-07-23 10:50:02 -0600

    feat: identacion

--- src/main/java/com/washtrack/washtrack_api/usuarios/dto/UsuarioEliminarReactivarDto.java
+++ src/main/java/com/washtrack/washtrack_api/usuarios/dto/UsuarioEliminarReactivarDto.java
@@ -17,9 +17,11 @@ public class UsuarioEliminarReactivarDto {
   @NotBlank(message = "Campo Id usuario requerido, favor de verificar")
   @Length(min = 10, max = 36, message = "El numero de caracteres es invalido al permitido para el Id")
   private String idUsuario;
+  
   @NotBlank(message = "Campo email es requerido, favor de verificar")
   @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$",
       message = "Formato de email no valido")
   private String email;
+  
   private String tenantId;
 }
